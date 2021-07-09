1822a201e04b707b1099eea7236fe63471757c75
NiklasRigelius <riggaan@gmail.com>
2021-07-09 11:17:42 +0200

    CRC - working

--- Client/src/Lab3.Client.c
+++ Client/src/Lab3.Client.c
@@ -22,6 +22,7 @@
 #include <sys/select.h>
 #include <unistd.h>
 
+
 #define msgLength 256
 
 struct Packet{
@@ -29,7 +30,7 @@ struct Packet{
 	int seq;
 	int id;
 	int windowSize;
-	int integerData;
+	int crc;
 	char data [msgLength];
 };
 
@@ -46,14 +47,19 @@ void goBackN(int _socket, struct sockaddr_in _server, struct Packet _header, cha
 int waitForResponse(int _socket, struct sockaddr_in _server, int _sec, int _usec, int _expectedSeq);
 
 char ** multiDimAllocation(int _row);
-void freeMulitDim(char ** _free, int _row);
+void freeMultiDim(char ** _free, int _row);
+
+int checkCRC(struct Packet _frame);
+void sendFrame(int _socket, struct sockaddr_in _server, int _flag, int _id, int _seq, int _windowSize, char * _msg);
+size_t buildCRCmsg(unsigned char * _CRCmsg, struct Packet _frame);
+int calculateCRC16(unsigned char _msg [], int length );
 
 int main(int argc, char *argv[]) {
 	puts("!!!Hello World 2!!!"); /* prints !!!Hello World!!! */
 
 	int _socket;
 	struct sockaddr_in _server;
-	struct Packet _handshake = {-1, -1, -1, -1, -1}; //JUNK values
+	struct Packet _handshake = {2, 2, 3, 1}; //JUNK values
 
 	char ** _inputData; //TODO: make sure its cleared
 	int _nrOfMsg = 0;
@@ -67,17 +73,22 @@ int main(int argc, char *argv[]) {
 
 	_inputData = userInteraction(&_handshake, &_nrOfMsg);
 
-	printf("This is your messages : \n");
-	for (int i = 0; i < _nrOfMsg; i++){
-		printf("Message %d : %s", i + 1, _inputData[i]);
-	}
+
 	connection(_socket, _server, &_handshake);
+
+//	unsigned char _CRCmsg [sizeof(struct Packet)];
+//	size_t _CRCmsgSize = buildCRCmsg((unsigned char *)&_CRCmsg, _handshake);
+//	_handshake.crc = calculateCRC16(_CRCmsg, _CRCmsgSize);
+//
+//	printf("crc = %d\n", _handshake.crc);
+//	return EXIT_SUCCESS;
+
 	printf("--------------------------------\n");
 	printf("Connected to the server\n");
 	printf("--------------------------------\n");
 	goBackN(_socket, _server, _handshake, _inputData, _nrOfMsg );
 
-
+	freeMultiDim(_inputData, _nrOfMsg);
 	return EXIT_SUCCESS;
 }
 
@@ -183,22 +194,6 @@ char ** userInteraction(struct Packet *_handshake, int  * _nrOfMsg){
 		_data[i][msgLength - 1] = '\0';
 	}
 
-//	//Fill integerData
-//	_validator = 1;
-//	while(_validator){
-//		printf("Enter number of integerData (more than 0): \n");
-//		scanf("%d", _nrOfintegerData);
-//		if(*_nrOfintegerData > 0) _validator = 0;
-//	}
-//	int * _integerData = (int *)malloc(sizeof(int) * (*_nrOfintegerData));
-//	if(_integerData == NULL){
-//		printf("ERROR, memory could not be allocated");
-//		exit(EXIT_FAILURE);
-//	}
-//	printf("Enter integerData in integers: \n");
-//	for(int i = 0; i < *_nrOfintegerData; i++){
-//		scanf("%d", &_integerData[i]);
-//	}
 	//TODO: CRS stuff
 
 	return _data;
@@ -211,20 +206,14 @@ void goBackN(int _socket, struct sockaddr_in _server, struct Packet _header,char
 
 	struct Packet _frame = _header;
 
+
 	while(!(_sentFrames == _nrOfMsg && _recvACK == _nrOfMsg)){
 		//If there is free space in window -> send frame
 		if(_windowsAvailable > 0 && _sentFrames != _nrOfMsg){
-			_frame.flags = 0;
-			_frame.seq = _end % _seqMax;
-			//_frame.integerData = _integerData[_end];
-			strcpy(_frame.data, _inputData[_end]);
-			sendToServer(_socket, _server, _frame);
-			_windowsAvailable --;
-			_end++;
-			_sentFrames++;
-			printf("Sending frame seq %d integerData %d\n", _frame.seq, _frame.integerData);
-			printf("Windows available %d\n", _windowsAvailable);
-			printf("------------------------------\n");
+			sendFrame(_socket, _server, 0, _header.id, (_end % _seqMax), _header.windowSize, _inputData[_end]);
+			_windowsAvailable --; 	//One frame is "on the move", there is one less window available
+			_end++;					//increase the _end variable to point to next data to be sent
+			_sentFrames++; 			//increase the amount of frames sent
 		}
 
 		int _recvStatus;
@@ -232,16 +221,16 @@ void goBackN(int _socket, struct sockaddr_in _server, struct Packet _header,char
 		//we are only interesting in ACKs and NAKs here, if timeout is triggered just continue
 		//The timer is set to 0 sec, 1 usec
 		_recvStatus = waitForResponse(_socket, _server, 0, 1, (_start % _seqMax));
-		if(_recvStatus == 1){  //recv an ACK
-			_start++;
-			_windowsAvailable++;
-			_recvACK ++;
+		if(_recvStatus == 1){  		//recv an ACK
+			_start++;				//increase the _start variable to point to the next data needing a ACK
+			_windowsAvailable++;	//Open up a slot for data to be sent
+			_recvACK ++;			//count the recv ACKs
 		}
-		else if(_recvStatus == -2){ //recv an NAK
-			_end = _start;
-			_windowsAvailable = _header.windowSize;
-			_sentFrames -= (_end - _start) + 1;
-			_recvACK = _sentFrames;
+		else if(_recvStatus == -2 || _recvStatus == -3){ //recv an NAK or corrupted packet TODO: Check if a corrupted packet should lead to windows being resent
+			_end = _start;			//reset _end to point to the start of the window -> will make sure the window is resent
+			_windowsAvailable = _header.windowSize;	//Make all slots in window available
+			_sentFrames -= (_end - _start) + 1;		//Subtract subtract _sentFrames with the number of frames needed to be resent
+			_recvACK = _sentFrames;					//Subtract subtract _recvAck with the number of frames needed to be resent
 		}
 
 		//If there is no windows available we need to wait for ACK, NAK or timeout
@@ -254,7 +243,7 @@ void goBackN(int _socket, struct sockaddr_in _server, struct Packet _header,char
 				_windowsAvailable++;
 				_recvACK ++;
 			}
-			else if(_recvStatus == -1 ||_recvStatus == -2){ //if timer is triggered or NAK was received
+			else if(_recvStatus == -1 ||_recvStatus == -2 || _recvStatus == -3){ //if timer is triggered or NAK was received
 				_end = _start;
 				_windowsAvailable = _header.windowSize;
 				_sentFrames -= (_end - _start) + 1;
@@ -292,11 +281,18 @@ int waitForResponse(int _socket, struct sockaddr_in _server, int _sec, int _usec
 		//recv msg -> check if correct flag and seq
 		recvFromServer(_socket, _server, &_recv);
 		if(_recv.flags == 1 && _recv.seq == _expectedSeq){ //move window to the "right"
-			printf("RECEIVED ACK: integerData %d, seq %d msg %s\n", _recv.integerData, _recv.seq, _recv.data);
-			return 1;
+			if(checkCRC(_recv) == 1){ //Packet not corrupted -> return 1
+
+				printf("RECEIVED ACK: seq %d data %s \n", _recv.seq, _recv.data);
+				return 1;
+			} else{ //Packet corrupted -> return -3;
+				printf("RECEIVED CORRUPTED: Packet was corrupted\n");
+				return -3;
+			}
+
 		}
 		else{ //resend window
-			printf("RECEIVED NAK: integerData %d, seq %d\n", _recv.integerData, _recv.seq);
+			printf("RECEIVED NAK: seq %d\n", _recv.seq);
 			return -2;
 		}
 	}
@@ -317,12 +313,70 @@ void sendToServer(int _socket, struct sockaddr_in _server, struct Packet _packet
 void recvFromServer(int _socket, struct sockaddr_in _server, struct Packet *_packet){
 	int _checker;
 	int _len = sizeof(_server);
-	printf("Here\n");
+
 	_checker = recvfrom(_socket, (struct Packet *)_packet, (sizeof(*_packet)), 0, (struct sockaddr *) &_server, &_len);
 	if(_checker < 0){
 	    perror("ERROR, Could not recv\n");
 	    exit(EXIT_FAILURE);
 	}
+}
+
+int checkCRC(struct Packet _frame){
+
+	int _calculatedCRC;
+	unsigned char _CRCmsg [sizeof(struct Packet)];
+	size_t _CRCmsgSize = buildCRCmsg((unsigned char *)&_CRCmsg, _frame);
+	_calculatedCRC = calculateCRC16(_CRCmsg, _CRCmsgSize);
+
+	if(_calculatedCRC == _frame.crc) return 1;
+	return 0;
+}
+
+void sendFrame(int _socket, struct sockaddr_in _server, int _flag, int _id, int _seq, int _windowSize, char * _msg){
+	struct Packet _frame;
+	_frame.flags = _flag;
+	_frame.id = _id;
+	_frame.seq = _seq;
+	_frame.windowSize = _windowSize;
+	strcpy(_frame.data, _msg);
+
+	unsigned char _CRCmsg [sizeof(struct Packet)];
+	size_t _CRCmsgSize = buildCRCmsg((unsigned char *)&_CRCmsg, _frame);
+	_frame.crc = calculateCRC16(_CRCmsg, _CRCmsgSize);
+
+	sendToServer(_socket, _server, _frame);
+}
+
+size_t buildCRCmsg(unsigned char * _CRCmsg, struct Packet _frame){
+	//Add header information and Data together to be a part of the crc calculation
+	//return the size of the unisgned char and the unsigned char containing the information
+	size_t _retSize = 0;
+
+	memcpy(&_CRCmsg[_retSize], &_frame.flags, sizeof(_frame.flags));
+	_retSize += sizeof(_frame.flags);
+	memcpy(&_CRCmsg[_retSize], &_frame.seq, sizeof(_frame.seq));
+	_retSize += sizeof(_frame.seq);
+	memcpy(&_CRCmsg[_retSize], &_frame.id, sizeof(_frame.id));
+	_retSize += sizeof(_frame.seq);
+	memcpy(&_CRCmsg[_retSize], &_frame.windowSize, sizeof(_frame.windowSize));
+	_retSize += sizeof(_frame.seq);
+	memcpy(&_CRCmsg[_retSize], &_frame.data, sizeof(_frame.data));
+	_retSize += sizeof(_frame.seq);
+
+	return _retSize;
+}
+
+int calculateCRC16(unsigned char _msg [], int length ){
+	//Calculate the CRC by performing polynomial division with the help of bitwise operators
+	int _crc = 0;
+	for(int i = 0; i < length; i++){	//for each byte
+		_crc ^= _msg[i];
+		for (unsigned k = 0; k < 8; k++){ //for each bit in a byte
+			_crc = _crc & 1 ? (_crc >> 1) ^ 0xa001 : _crc >> 1;
+
+		}
+	}
+	return _crc;
 }
 
 char ** multiDimAllocation(int _row){
@@ -334,7 +388,7 @@ char ** multiDimAllocation(int _row){
 	return _retArr;
 }
 
-void freeMulitDim(char ** _free, int _row){
+void freeMultiDim(char ** _free, int _row){
 	for(int i = 0; i < _row; i++){
 		free(_free[i]);
 	}
--- Server/src/Lab3.Server.c
+++ Server/src/Lab3.Server.c
@@ -24,7 +24,7 @@ struct Packet{
 	int seq;
 	int id;
 	int windowSize;
-	int integerData;
+	int crc;
 	char data [msgLength];
 };
 
@@ -36,6 +36,11 @@ void sendToClient(int _socket, struct sockaddr_in _client, struct Packet _packet
 struct sockaddr_in connection(int _socket, struct Packet *_handshake);
 
 void goBackN(int _socket,  struct sockaddr_in _client, int _seqMax);
+
+int checkCRC(struct Packet _frame);
+void sendFrame(int _socket, struct sockaddr_in _server, int _flag, int _id, int _seq, int _windowSize, char * _msg);
+size_t buildCRCmsg(unsigned char * _CRCmsg, struct Packet _frame);
+int calculateCRC16(unsigned char _msg [], int length );
 
 int main(int argc, char *argv[]) {
 	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
@@ -96,7 +101,7 @@ struct sockaddr_in connection(int _socket, struct Packet *_handshake){
 			case 0:
 				//wait for SYNC
 				recvFromClient(_socket, &_client, _handshake);
-				printf("recv SYNC %d integerData %d\n", _handshake->flags, _handshake->integerData);
+				printf("recv SYNC %d\n", _handshake->flags);
 				_status++;
 				break;
 			case 1:
@@ -140,26 +145,91 @@ void goBackN(int _socket, struct sockaddr_in _client, int _seqMax){
 	FD_ZERO(&_fdSet);
 	FD_SET(_socket, &_fdSet);
 
-	struct Packet * _frame = (struct Packet*)malloc(sizeof(struct Packet));  //Junk
+	struct Packet * _frame = (struct Packet*)malloc(sizeof(struct Packet));
+
 	while(_send){
 		sleep(1);
 		recvFromClient(_socket, &_client, _frame);
 		if(_frame->seq != _expectedSeq){
-			//Wrong seq
-			printf("SENDING NAK: Wrong seq %d (expected %d) integerData %d\n", _frame->seq, _expectedSeq, _frame->integerData);
-			_frame->flags = 4;
-			sendToClient(_socket, _client, *_frame);
+			//Wrong seq -> send NAK
+			sendFrame(_socket, _client, 4, _frame->id, _frame->seq, _frame->windowSize, _frame->data);
+			printf("SENDING NAK : Received packet had wrong sequence.");
 			printf("-----------------\n");
 		} else if(_frame->seq == _expectedSeq){
-			printf("SENDING ACK: Recv integerData %d, seq %d msg %s\n", _frame->integerData, _frame->seq, _frame->data);
-			_frame->flags = 1;
-			_frame->seq = _expectedSeq;
-			strncpy(_frame->data, "HEJ Niklas, jag heter Sven", 26);
-			sendToClient(_socket, _client, *_frame);
-			_expectedSeq = (_expectedSeq + 1) % _seqMax;
+			//Crc check
+			int _checker = checkCRC(*_frame);
+			if(_checker == 1){ //Not corrupted -> send ACK
+				sendFrame(_socket, _client, 1, _frame->id, _expectedSeq, _frame->windowSize, _frame->data);
+				printf("SENDING ACK: Received packet was valid. \n");
+				_expectedSeq = (_expectedSeq + 1) % _seqMax;
+			}
+			else { //Corrupted -> send NAK
+				//TODO: Should corurpted packets be NAKed or just discarded?
+				sendFrame(_socket, _client, 4, _frame->id, _frame->seq, _frame->windowSize, _frame->data);
+				printf("SENDING NAK : Received packet was corrupted.");
+				printf("-----------------\n");
+			}
+
 		}
 		printf("-----------------\n");
 	}
+}
+
+void sendFrame(int _socket, struct sockaddr_in _server, int _flag, int _id, int _seq, int _windowSize, char * _msg){
+	struct Packet _frame;
+	_frame.flags = _flag;
+	_frame.id = _id;
+	_frame.seq = _seq;
+	_frame.windowSize = _windowSize;
+	strcpy(_frame.data, _msg);
+
+	unsigned char _CRCmsg [sizeof(struct Packet)];
+	size_t _CRCmsgSize = buildCRCmsg((unsigned char *)&_CRCmsg, _frame);
+	_frame.crc = calculateCRC16(_CRCmsg, _CRCmsgSize);
+
+	sendToClient(_socket, _server, _frame);
+}
+
+int checkCRC(struct Packet _frame){
+
+	int _calculatedCRC;
+	unsigned char _CRCmsg [sizeof(struct Packet)];
+	size_t _CRCmsgSize = buildCRCmsg((unsigned char *)&_CRCmsg, _frame);
+	_calculatedCRC = calculateCRC16(_CRCmsg, _CRCmsgSize);
+
+	if(_calculatedCRC == _frame.crc) return 1;
+	return 0;
+}
+
+size_t buildCRCmsg(unsigned char * _CRCmsg, struct Packet _frame){
+	//Add header information and Data together to be a part of the crc calculation
+		//return the size of the unisgned char and the unsigned char containing the information
+		size_t _retSize = 0;
+
+		memcpy(&_CRCmsg[_retSize], &_frame.flags, sizeof(_frame.flags));
+		_retSize += sizeof(_frame.flags);
+		memcpy(&_CRCmsg[_retSize], &_frame.seq, sizeof(_frame.seq));
+		_retSize += sizeof(_frame.seq);
+		memcpy(&_CRCmsg[_retSize], &_frame.id, sizeof(_frame.id));
+		_retSize += sizeof(_frame.seq);
+		memcpy(&_CRCmsg[_retSize], &_frame.windowSize, sizeof(_frame.windowSize));
+		_retSize += sizeof(_frame.seq);
+		memcpy(&_CRCmsg[_retSize], &_frame.data, sizeof(_frame.data));
+		_retSize += sizeof(_frame.seq);
+
+		return _retSize;
+}
+
+int calculateCRC16(unsigned char _msg [], int length ){
+	//Calculate the CRC by performing polynomial division with the help of bitwise operators
+	int _crc = 0;
+	for(int i = 0; i < length; i++){	//for each byte
+		_crc ^= _msg[i];
+		for (unsigned k = 0; k < 8; k++){ //for each bit in a byte
+			_crc = _crc & 1 ? (_crc >> 1) ^ 0xa001 : _crc >> 1;
+		}
+	}
+	return _crc;
 }
 
 void sendToClient(int _socket, struct sockaddr_in _client, struct Packet _packet){
